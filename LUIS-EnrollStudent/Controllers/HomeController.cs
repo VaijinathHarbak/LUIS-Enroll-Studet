2d3c96d8f7a745d1f7e1d6c1b40ffa37c9577fec
Vaijinath.harbak <nath.harbak@gmail.com>
2017-04-05 19:49:27 +0530

    added the initial LUIS-Enroll-Student

---
+++ LUIS-EnrollStudent/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using LUIS_EnrollStudent.Models;
+using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Configuration;
+using Newtonsoft.Json;
+namespace LUISEnrollStudent.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region public async Task<ActionResult> Index(string searchString)
+        public async Task<ActionResult> Index(string searchString)
+        {
+            Query Return = new Query();
+            try
+            {
+                if (searchString != null)
+                {
+                    LUIS objLUISResult = await QueryLUIS(searchString);
+                    foreach (var item in objLUISResult.entities)
+                    {
+                        if (item.type == "Student Name::First Name")
+                        {
+                            Return.FirstName = item.entity;
+                        }
+                        if (item.type == "Student Name::Last Name")
+                        {
+                            Return.LastName = item.entity;
+                        }
+                        if (item.type == "Class")
+                        {
+                            Return.Class = item.entity;
+                        }
+                        if (item.type == "Period")
+                        {
+                            Return.Period = item.entity;
+                        }
+                    }
+                }
+                return View(Return);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error: " + ex);
+                return View(Return);
+            }
+        }
+        #endregion
+        // Utility
+        #region private static async Task<Example> QueryLUIS(string Query)
+        private static async Task<LUIS> QueryLUIS(string Query)
+        {
+            LUIS LUISResult = new LUIS();
+            var LUISQuery = Uri.EscapeDataString(Query);
+            using (System.Net.Http.HttpClient client = new System.Net.Http.HttpClient())
+            {
+                // Get key values from the web.config
+                string LUIS_Url = WebConfigurationManager.AppSettings["LUISUrl"];
+                string LUIS_Subscription_Key = WebConfigurationManager.AppSettings["LUISSubscriptionKey"];
+                string RequestURI = String.Format("{0}?subscription-key={1}&q={2}",
+                    LUIS_Url, LUIS_Subscription_Key, LUISQuery);
+                System.Net.Http.HttpResponseMessage msg = await client.GetAsync(RequestURI);
+                if (msg.IsSuccessStatusCode)
+                {
+                    var JsonDataResponse = await msg.Content.ReadAsStringAsync();
+                    LUISResult = JsonConvert.DeserializeObject<LUIS>(JsonDataResponse);
+                }
+            }
+            return LUISResult;
+        }
+        #endregion
+    }
+}
---
+++ LUIS-EnrollStudent/Models/LUIS.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LUIS_EnrollStudent.Models
+{
+    public class TopScoringIntent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public double score { get; set; }
+    }
+
+    public class LUIS
+    {
+        public string query { get; set; }
+        public TopScoringIntent topScoringIntent { get; set; }
+        public IList<Intent> intents { get; set; }
+        public IList<Entity> entities { get; set; }
+    }
+
+    public class Query
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Class { get; set; }
+        public string Period { get; set; }
+    }
+}
